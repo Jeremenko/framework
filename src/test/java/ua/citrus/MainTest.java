aa33211e92e871bf52851daac2856070319db8f1
Admin <komarik100@gmail.com>
2020-05-19 22:23:58 +0300

    add to card

--- src/test/java/ua/citrus/MainTest.java
+++ src/test/java/ua/citrus/MainTest.java
@@ -56,19 +56,39 @@ public class MainTest extends Settings {
         phoneType.moveToElement(linkPhones).build().perform();
 
         WebElement xiaomiPhones = driver.findElement(By.xpath(
-                "//li[@class='menu-aim__item menu-aim__item--active']//div[@class='wrap']//div[1]//ul[1]//li[5]//a[1]//span[1]"));
+                "//li[@class='menu-aim__item menu-aim__item--active']//span[text()='Xiaomi']"));
         xiaomiPhones.click();
-        WebElement xiaomiPhonesPage = driver.findElement(By.xpath("//h1[contains(text(),'Xiaomi')]"));
 
+        WebElement xiaomiPhonesPage = driver.findElement(By.xpath("//h1[contains(text(),'Xiaomi')]"));
         assertEquals(xiaomiPhonesPage.getText(), "Смартфоны Xiaomi", "You are in different page");
 
-
-        List<WebElement> phonesPrices = driver.findElements(By.xpath("//div[@class='product-card__overview'][.//div[@class='product-card__footer']//button[@class='product-card__to-basket']]//div[@class='prices__price']//span[@class='price']"));
         WebElement phoneWithMaxPrice = null;
         int maxPrice = 0;
+//----------------------------------------New piece of code----------------------------
+        List<WebElement> allItems = driver.findElements(By.xpath("//div[@class='product-card__body']"));
+        for (WebElement item : allItems) {
+            WebElement phonePrice = item.findElement(By.xpath("[.//div[@class='prices__price']//span[@class='price']"));
+
+            String deleteSpaceInPrice = phonePrice.getText().replace(" ", "");
+            int pricePhoneInt = Integer.parseInt(deleteSpaceInPrice); //выводит список прайсов как цыфры и без пробелов готов к сравнению
+
+            if (maxPrice < pricePhoneInt) {
+                maxPrice = pricePhoneInt;
+                phoneWithMaxPrice = item;
+            }
+        }
+        WebElement basket = phoneWithMaxPrice.findElement(By.xpath("//div[@class='product-card__overview']//div[@class='product-card__footer']//button[@class='product-card__to-basket']"));
+        basket.click();
+        Thread.sleep(20000); // не бейте ногами хотел увидеть попал в корзинку тел или нет
+
+
+
+//---------------------------------------------------------------------
+//--------------------------------------------------------------------
+        List<WebElement> phonesPrices = driver.findElements(By.xpath("//div[@class='product-card__overview'][.//div[@class='product-card__footer']//button[@class='product-card__to-basket']]//div[@class='prices__price']//span[@class='price']"));
+
         for (WebElement phonePrice : phonesPrices) {
             String deleteSpaceInPrice = phonePrice.getText().replace(" ", "");
-
             int pricePhoneInt = Integer.parseInt(deleteSpaceInPrice); //выводит список прайсов как цыфры и без пробелов готов к сравнению
 
             if (maxPrice < pricePhoneInt) {
@@ -77,8 +97,7 @@ public class MainTest extends Settings {
             }
         }
         System.out.println(maxPrice);
-
-
+//---------------------------------------------------------------------
 
 //        WebElement basket = phoneWithMaxPrice.findElement(By.xpath("//div[@class='product-card__overview']//button[@class='product-card__to-basket']"));
 //        Thread.sleep(10000);
